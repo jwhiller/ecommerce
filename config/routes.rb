b0eb30ff6be8a6913d96e9bb8a4fee93c952552f
James Hiller <hillerj1@gmail.com>
2018-07-16 10:11:41 -0400

    add modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'categorical' => 'storefront#items_by_category'
+  get 'branding' => 'storefront#items_by_brand'
+
   resources :categories
   resources :products
 
+  root 'storefront#all_items'
 
-  root 'products#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
