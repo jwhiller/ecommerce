2827281337813af464dcfa0e444082e4ae45d021
James Hiller <hillerj1@gmail.com>
2018-07-17 09:14:39 -0400

    partial created for storefront

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :categories, :brands
+
+  def categories
+    @categories = Category.all
+  end
+
+  def brands
+      @brands = Product.pluck(:brand).sort.uniq
+    end
 end
