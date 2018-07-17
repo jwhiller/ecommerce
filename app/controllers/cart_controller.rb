acbae68f22425120f0dd3b55f9f6dd2464436327
James Hiller <hillerj1@gmail.com>
2018-07-17 17:52:24 -0400

    add to cart and jQuery flash

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,11 +3,17 @@ class CartController < ApplicationController
 before_action :authenticate_user!, only: [:checkout]
 
   def add_to_cart
-
     line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
-      	line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
 
-redirect_to root_url
+    if params[:quantity].blank?
+      flash[:error] = "Select quantity for your #{line_item.product.name}!"
+      line_item.destroy
+      redirect_to root_url
+    else
+      line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+      redirect_to root_url
+    end
+
   end
 
   def view_order
@@ -19,6 +25,32 @@ line_item.destroy
 redirect_to view_order_path
 end
 
-  def checkout
+def checkout
+  line_items = LineItem.all
+
+    if line_items.empty?
+      redirect_to root_url
+    else
+  @order = Order.create(user_id: current_user.id, subtotal: 0)
+
+  line_items.each do |line_item|
+    line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+
+      if @order.order_items[line_item.product_id].nil?
+        @order.order_items[line_item.product_id] = line_item.quantity
+      else
+        @order.order_items[line_item.product_id] += line_item.quantity
+      end
+
+      @order.subtotal += line_item.line_item_total
+
   end
+    @order.save
+
+    @order.update(sales_tax: (@order.subtotal * 0.08))
+    @order.update(grand_total: (@order.sales_tax + @order.subtotal))
+
+    line_items.destroy_all
+end
+end
 end
